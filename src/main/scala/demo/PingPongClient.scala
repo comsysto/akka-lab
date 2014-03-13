ca474aabca333bc2d8952de1fbd97ef866415c91
Daniel Mitterdorfer <daniel.mitterdorfer@comsysto.com>
2014-03-13 10:51:35 +0100

    Compile fixes after merge

--- src/main/scala/demo/PingPongClient.scala
+++ src/main/scala/demo/PingPongClient.scala
@@ -1,8 +1,7 @@
 package demo
 
-import akka.actor.{ActorRef, Props, ActorSystem}
+import akka.actor.{Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
-import com.comsysto.trading.akka.remoting.PingPongActor
 
 object PingPongClient extends App {
   val sys = ActorSystem("PingPong", ConfigFactory.load("application-remoting.conf"))
